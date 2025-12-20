6bb640903c811714654e31c4424d4f53c13efe46
Raiyyan Israili <ryyn.israili@Raiyyans-MacBook-Air.local>
2025-12-21 03:48:49 +0530

    Basic working with miliseconds

--- index.js
+++ index.js
@@ -4,44 +4,62 @@ var start = document.getElementById("start");
 var reset = document.getElementById("reset");
 
 
-let currentSec = 0;
-let currentMin = 0;
-let currentHour = 0;
+let miliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
-var interval;
+var interval = null;
 start.addEventListener("click", function () {
+    if (interval !== null) {
+        start.innerHTML = 'Start';
+        clearInterval(interval);
+        interval = null;
 
+        return;
+    }
+
+    start.innerHTML = 'Stop';
     interval = setInterval(function () {
-        display.innerHTML = `${formatVal(currentHour)}: ${formatVal(currentMin)}: ${formatVal(currentSec)}`;
         updateTime();
-    }, 1000);
+        display.innerHTML = `${formatVal(hours)} : ${formatVal(minutes)} : ${formatVal(seconds)} : ${formatVal(miliseconds)}`;
+
+    }, 10);
 });
 
 function updateTime() {
-    currentSec += 1;
+    miliseconds += 1;
 
-    if (currentSec > 5) {
-        currentSec = 0;
-        currentMin += 1;
+    if (miliseconds > 99) {
+        miliseconds = 0;
+        seconds += 1;
 
-        if (currentMin > 5) {
-            currentMin = 0;
-            currentHour += 1;
+        if (seconds > 59) {
+            seconds = 0;
+            minutes += 1;
+
+            if (minutes > 59) {
+                minutes = 0;
+                hours += 1;
+            }
         }
     }
 }
 
 reset.addEventListener("click", function () {
+    start.innerHTML = 'Start';
     clearInterval(interval);
-    currentSec = 0;
-    currentMin = 0;
-    currentHour = 0;
-    display.innerHTML = '00: 00: 00';
+    interval = null;
+
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    display.innerHTML = '00 : 00 : 00 : 00';
 });
 
 function formatVal(unit) {
     if (unit < 10) {
         return '0' + unit;
-    } 
+    }
     return unit;
 }
