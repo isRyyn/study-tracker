8d69e90119b7c7cf9758e1a4aca727d33c9598de
Raiyyan Israili <rynnisraily@gmail.com>
2025-12-21 16:43:17 +0530

    Updated manual to date.now

--- index.js
+++ index.js
@@ -1,65 +1,83 @@
-let display = document.getElementById("display");
+// Elements
+var display = document.getElementById("display");
 
-var start = document.getElementById("start");
-var reset = document.getElementById("reset");
+var playBtn = document.getElementById("playBtn");
+var resetBtn = document.getElementById("resetBtn");
 
 
+
+// Time
 let miliseconds = 0;
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
+
+// Vars
+let startTime = null;
+let diff = null;
+let isPaused = false;
+
 var interval = null;
-start.addEventListener("click", function () {
+playBtn.addEventListener("click", function () {
+    
     if (interval !== null) {
-        start.innerHTML = 'Start';
-        clearInterval(interval);
-        interval = null;
+        diff = Date.now() - startTime;
+        playBtn.innerHTML = 'Start';
+        resetInterval();
+
+        isPaused = true;
 
         return;
     }
+    
 
-    start.innerHTML = 'Stop';
+    startTime = Date.now() - (isPaused ? diff : 0);
+    isPaused = false;
+    playBtn.innerHTML = 'Lap';
     interval = setInterval(function () {
         updateTime();
-        display.innerHTML = `${formatVal(hours)} : ${formatVal(minutes)} : ${formatVal(seconds)} : ${formatVal(miliseconds)}`;
+        display.innerHTML = `${formatVal(hours)} : ${formatVal(minutes)} : ${formatVal(seconds)} : ${formatMiliseconds(miliseconds)}`;
 
-    }, 10);
+    }, 1);
+});
+ 
+resetBtn.addEventListener("click", function () {
+    playBtn.innerHTML = 'Start';
+    resetInterval();
+
+    display.innerHTML = '00 : 00 : 00 : 000';
 });
 
-function updateTime() {
-    miliseconds += 1;
-
-    if (miliseconds > 99) {
-        miliseconds = 0;
-        seconds += 1;
-
-        if (seconds > 59) {
-            seconds = 0;
-            minutes += 1;
-
-            if (minutes > 59) {
-                minutes = 0;
-                hours += 1;
-            }
-        }
-    }
-}
-
-reset.addEventListener("click", function () {
-    start.innerHTML = 'Start';
+function resetInterval() {
     clearInterval(interval);
     interval = null;
+}
 
-    seconds = 0;
-    minutes = 0;
-    hours = 0;
-    display.innerHTML = '00 : 00 : 00 : 00';
-});
+
+function updateTime() {
+    diff = Date.now() - startTime;
+
+    miliseconds = diff % 1000;    
+    seconds = Math.floor(diff / 1000) % 60;
+    minutes = Math.floor(diff / 60000) % 60;
+    hours = Math.floor(diff/ 3600000);
+     
+}
 
 function formatVal(unit) {
     if (unit < 10) {
         return '0' + unit;
     }
+    return unit;
+}
+
+
+function formatMiliseconds(unit) {
+    if (unit < 10) {
+        return '00' + unit;
+    } else if (unit < 100) {
+        return '0' + unit;
+    } 
     return unit;
 }
